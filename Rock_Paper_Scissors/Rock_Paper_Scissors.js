e0a7186b934ceeb6049d41e49651b39f19e0852a
Kenia de Oliveira <keniapubg@gmail.com>
2023-07-08 16:22:52 -0300

    Coloquei a condição de entrada

--- Rock_Paper_Scissors/Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.js
@@ -1,32 +1,36 @@
 const prompt = require('prompt-sync')();
 let maquina =[];
 
-let choice = prompt('\tRock(1)\t   Paper(2)\tScissors(3)\t\nMake Your Choice:');
+let choice = parseInt(prompt('\tRock(1)\t   Paper(2)\tScissors(3)\t\nMake Your Choice:'));
 
 function numero(a, b){
     return Math.floor(Math.random()*(b - a + 1)) + a;
 }
+
 maquina = numero(1, 3);
+
+while (![1, 2, 3].includes(choice)) {
+    console.log('Escolha Invalida!!! Digite uma opção valida')
+    choice = parseInt(prompt('\tRock(1)\t   Paper(2)\tScissors(3)\t\nMake Your Choice:'));
+}
 
 function escolha(choice, maquina) {
     let mensagem = '';
+    
     if (choice==1) {
         mensagem += 'You chose ROCK\n'
-    }else{
-        if (choice==2) {
-            mensagem+='You chose PAPER\n'
-        } else {
-            mensagem+='You chose SCISSORS\n'
-        }
+    }else if (choice==2) {
+        mensagem+='You chose PAPER\n'
+    }else {
+        mensagem+='You chose SCISSORS\n'
     }
+
     if (maquina==1) {
         mensagem+='Computer chose ROCK\n'
-    }else{
-        if (maquina==2) {
-            mensagem+='Computer chose PAPER\n'
-        } else {
-            mensagem+='Computer chose SCISSORS\n'
-        }
+    }else if (maquina==2) {
+        mensagem+='Computer chose PAPER\n'
+    } else {
+        mensagem+='Computer chose SCISSORS\n'
     }
     return mensagem;
 }
@@ -40,13 +44,13 @@ else{
     if(choice>maquina){ 
         if (choice-maquina==1) {
             console.log('YOU WIN! :D\n');
-        } else {(choice-maquina==2)
+        }else {(choice-maquina==2)
             console.log('YOU LOSE! :(\n');
         }
     }else{
         if (maquina-choice==1) {
             console.log('YOU LOSE! :(\n');
-        } else {(maquina-choice==2)
+        }else {(maquina-choice==2)
             console.log('YOU WIN! :D\n');
         }
     }
