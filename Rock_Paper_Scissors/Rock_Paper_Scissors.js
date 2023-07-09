8289d0b0672084efb9c6b5b478b2305bb6d18111
Kenia de Oliveira <keniapubg@gmail.com>
2023-07-09 09:21:09 -0300

    Modularização e adionando comentario

--- Rock_Paper_Scissors/Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.js
@@ -1,19 +1,19 @@
 const prompt = require('prompt-sync')();
-let maquina =[];
-
 let choice = parseInt(prompt('\tRock(1)\t   Paper(2)\tScissors(3)\t\nMake Your Choice:'));
 
+//Condição de entrada para entrar inserir apenas numeros válidos
+while (![1, 2, 3].includes(choice)) {
+    console.log('Escolha Inválida!!! Digite uma opção válida');
+    choice = parseInt(prompt('\tRock(1)\t   Paper(2)\tScissors(3)\t\nMake Your Choice:'));
+}
+//escolha da maquina: para sortear um numero aleatorio entre 'a' e 'b'
 function numero(a, b){
     return Math.floor(Math.random()*(b - a + 1)) + a;
 }
+//atribuir o numero apenas de 1 a 3
+let maquina = numero(1, 3);
 
-maquina = numero(1, 3);
-
-while (![1, 2, 3].includes(choice)) {
-    console.log('Escolha Invalida!!! Digite uma opção valida')
-    choice = parseInt(prompt('\tRock(1)\t   Paper(2)\tScissors(3)\t\nMake Your Choice:'));
-}
-
+//exibir a mensagem de escolha do usuario e da maquina atribuindo o nome aos numeros escolhidos
 function escolha(choice, maquina) {
     let mensagem = '';
     
@@ -24,7 +24,6 @@ function escolha(choice, maquina) {
     }else {
         mensagem+='You chose SCISSORS\n'
     }
-
     if (maquina==1) {
         mensagem+='Computer chose ROCK\n'
     }else if (maquina==2) {
@@ -37,21 +36,15 @@ function escolha(choice, maquina) {
 
 console.log(escolha(choice, maquina));
 
-if (choice-maquina==0 || maquina-choice==0) {
-    console.log('DRAW! :|\n');
-}
-else{
-    if(choice>maquina){ 
-        if (choice-maquina==1) {
-            console.log('YOU WIN! :D\n');
-        }else {(choice-maquina==2)
-            console.log('YOU LOSE! :(\n');
-        }
-    }else{
-        if (maquina-choice==1) {
-            console.log('YOU LOSE! :(\n');
-        }else {(maquina-choice==2)
-            console.log('YOU WIN! :D\n');
-        }
+//essa função determina o resultado do jogo
+function Resultado(choice, maquina) {
+    if (choice-maquina==0 || maquina-choice==0) {
+        return 'DRAW! :|\n';//retorna empate se a diferença entre as escolha for 0
+    }
+    else if(choice>maquina){ 
+        return (choice-maquina==1) ? 'YOU WIN! :D\n' : 'YOU LOSE! :(\n';//retornar vitória se a diferença for 1, senão retorna derrota
+    }else{ 
+        return (maquina-choice==1) ? 'YOU WIN! :D\n' : 'YOU LOSE! :(\n'//retornar vitória se a diferença for 1, senão retorna derrota
     }
 }
+console.log(Resultado(choice, maquina));
